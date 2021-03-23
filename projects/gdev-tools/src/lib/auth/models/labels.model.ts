7434acc78ac10d7e5ac048047fcb286a8ae7a0db
jgu7man <jgu7man@gmail.com>
2021-03-23 15:45:39 -0600

    replaced tools for submodule

--- projects/gdev-tools/src/lib/auth/models/labels.model.ts
+++
@@ -1,8 +0,0 @@
-export interface GdevRestorePasswordLabels {
-  emailLabel: string
-  exampleLabel: string
-  requiredLabel: string
-  cancelButtonLabel: string
-  sendButtonLabel: string
-  confirmationMessage: string
-}
--- projects/gdev-tools/src/lib/sidenav/gdev-sidenav.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface GdevSidenavNode {
-  name: string,
-  routeId?: string | string[],
-  route?: string,
-  childs?: GdevSidenavNode[],
-  disable?: boolean
-}
